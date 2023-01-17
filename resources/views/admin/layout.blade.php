f9f6cb267ca59299390504be4e11f66d52844aa8
自分の名前 <@example.com>
2023-01-17 16:29:38 +0000

    管理画面デザインにBootstrap5を入れた

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -3,10 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <title>ログイン機能付きタスク管理サービス 管理画面 @yield('title')</title>
     </head>
     <body>
-        @auth('admin')
+@auth('admin')
         <menu label="リンク">
         <a href="/admin/user/list">ユーザ一覧</a><br>
         管理画面機能 1<br>
@@ -17,5 +18,6 @@
         </menu>
 @endauth
 @yield('contets')
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>            
     </body>
 </html>
